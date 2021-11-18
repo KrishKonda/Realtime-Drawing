3df625794f09f3482d07234fe28bbc3c2c7da92c
KrishKonda <77596750+KrishKonda@users.noreply.github.com>
2021-11-17 20:42:39 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function preload(){
+
+}
+function setup(){
+video=createCapture(VIDEO);
+video.size(550,500);
+canvas=createCanvas(550,500);
+canvas.position(560,150);
+posenet=ml5.poseNet(video,modelLoaded);
+posenet.on("pose",gotPoses);
+}
+function draw(){
+    background("#ff6600");
+}
+function modelLoaded(){
+    console.log("poseNet model is initialized");
+}
+function gotPoses(results){
+    if(results.length>0){
+        console.log(results);
+    }
+}
